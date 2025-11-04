6c5e12b6191582115eb0c755a6e3a6ba9f2c564c
ankit <ujjawalankit56023@gmail.com>
2025-11-04 19:26:05 +0530

    prod 1

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
   },
-  server: {
-    historyApiFallback: true
-  }
 });
+
 
